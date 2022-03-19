626920851db9c77d4a1c43bd2377ad521e490938
Kushan <ra.kushan1997@gmail.com>
2022-03-19 15:48:46 +0530

    changes in chat clinet service and coodrdination service

--- src/main/java/Services/ChatService/ChatClientService.java
+++ src/main/java/Services/ChatService/ChatClientService.java
@@ -1,29 +1,56 @@
 package Services.ChatService;
 import Models.Server.ServerData;
 import Models.Server.ServerState;
+import org.apache.log4j.Logger;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ChatClientService implements Runnable {
     private static ChatClientService ChatClientInstance;
+    private Socket clientSocket;
     private final int port;
     private static String id;
+    private  Logger logger = Logger.getLogger(ChatClientService.class);
+    private DataInputStream ournewDataInputstream;
 
     private ChatClientService(){
         ServerData server_info = ServerState.getServerStateInstance().getServerData();
         this.port = server_info.getClientPort();
         this.id = server_info.getServerID();
+        try {
+            InetAddress inetadress = InetAddress.getByName(server_info.getServerAddress());
+            ServerSocket serverClientSocket = new ServerSocket(this.port);
+            this.clientSocket = serverClientSocket.accept();
+            ournewDataInputstream = new DataInputStream(clientSocket.getInputStream());
+            System.out.println(clientSocket);
+        }catch (Exception e){
+            logger.error("ChatClientService exception occurred. Reason - "+ e.getMessage());
+        }
     }
+
     public static ChatClientService getInstance(){
         if (ChatClientInstance== null){
             ChatClientInstance = new ChatClientService();
         }
         return ChatClientInstance;
     }
+
     @Override
     public void run() {
         while (true){
-            try{Thread.sleep(1500);}catch(InterruptedException e){System.out.println(e);}
-                System.out.println("server id = " +this.id);
-                System.out.println("server port = " +this.port);
+            try {
+                String receivedString = this.ournewDataInputstream.readUTF();
+                System.out.println("Received message = " + receivedString);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+//            try{Thread.sleep(1500);}catch(InterruptedException e){System.out.println(e);}
+//                System.out.println("server id = " +this.id);
+//                System.out.println("server port = " +this.port);
             }
     }
 }
--- src/main/java/Services/CoordinationService/CoordinationService.java
+++ src/main/java/Services/CoordinationService/CoordinationService.java
@@ -2,16 +2,26 @@ package Services.CoordinationService;
 
 import Models.Server.ServerData;
 import Models.Server.ServerState;
+import org.apache.log4j.Logger;
+
+import java.net.Socket;
 
 public class CoordinationService implements Runnable{
     private static CoordinationService CoordinationInstance;
     private final int port;
     private static String id;
+    private Socket coordinationSocket;
+    Logger logger = Logger.getLogger(CoordinationService.class);
 
     private CoordinationService(){
         ServerData server_info = ServerState.getServerStateInstance().getServerData();
         this.port = server_info.getCoordinationPort();
         this.id = server_info.getServerID();
+        try {
+            this.coordinationSocket = new Socket(server_info.getServerAddress(), this.port);
+        }catch (Exception e){
+            logger.error("Coordination exception occurred. Reason - "+ e.getMessage());
+        }
     }
     public static CoordinationService getInstance(){
         if (CoordinationInstance== null){
@@ -23,8 +33,8 @@ public class CoordinationService implements Runnable{
     public void run() {
         while (true){
             try{Thread.sleep(1500);}catch(InterruptedException e){System.out.println(e);}
-            System.out.println("server id = " +this.id);
-            System.out.println("server coordination port = " +this.port);
+//            System.out.println("server id = " +this.id);
+//            System.out.println("server coordination port = " +this.port);
         }
     }
 }
